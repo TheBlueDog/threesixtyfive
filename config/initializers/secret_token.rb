632fb0ce7f2dd4570e685275ad0a98d96b5eb0f1
TheBlueDog <thebluedog@gmail.com>
2013-12-29 14:01:44 -0500

    Baseline Rails App initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threesixtyfive::Application.config.secret_key_base = '642a4693d7aca22cf0dffff3aabc1560fcfc45ba5434673ca3b54e97831548116bf9cee65ffa91f28c76bdefcb3a610d14b6b30d0750273b56045442a901d19d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Threesixtyfive::Application.config.session_store :cookie_store, key: '_threesixtyfive_session'
