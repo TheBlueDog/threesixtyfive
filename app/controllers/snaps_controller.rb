274816a67907eab181b63fcaaa140d9062da24d9
TheBlueDog <thebluedog@gmail.com>
2013-12-29 22:24:42 -0500

    Updates Snapss views

--- app/controllers/snaps_controller.rb
+++ app/controllers/snaps_controller.rb
@@ -1,28 +1,20 @@
 class SnapsController < ApplicationController
   before_action :set_snap, only: [:show, :edit, :update, :destroy]
 
-  # GET /snaps
-  # GET /snaps.json
   def index
     @snaps = Snap.all
   end
 
-  # GET /snaps/1
-  # GET /snaps/1.json
   def show
   end
 
-  # GET /snaps/new
   def new
     @snap = Snap.new
   end
 
-  # GET /snaps/1/edit
   def edit
   end
 
-  # POST /snaps
-  # POST /snaps.json
   def create
     @snap = Snap.new(snap_params)
 
@@ -37,8 +29,6 @@ class SnapsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /snaps/1
-  # PATCH/PUT /snaps/1.json
   def update
     respond_to do |format|
       if @snap.update(snap_params)
@@ -51,8 +41,6 @@ class SnapsController < ApplicationController
     end
   end
 
-  # DELETE /snaps/1
-  # DELETE /snaps/1.json
   def destroy
     @snap.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class SnapsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_snap
       @snap = Snap.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def snap_params
       params.require(:snap).permit(:title, :description)
     end
--- app/views/snaps/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@snaps) do |snap|
-  json.extract! snap, :id, :title, :description
-  json.url snap_url(snap, format: :json)
-end
