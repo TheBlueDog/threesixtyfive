4ee5613205a5e688382efa46f2ebe76dc059bfd9
TheBlueDog <thebluedog@gmail.com>
2013-12-31 00:19:47 -0500

    Add image uploading function with Paperclip

--- app/models/snap.rb
+++ app/models/snap.rb
@@ -1,3 +1,4 @@
 class Snap < ActiveRecord::Base
 	belongs_to :user
+	has_attached_file :image, :styles => { :xlarge => "2048x2048>", :large => "1024x1024>", :medium => "800x800>", :small => "320x320>", :thumb => "150x150>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131231003129) do
+ActiveRecord::Schema.define(version: 20131231044657) do
 
   create_table "snaps", force: true do |t|
     t.string   "title"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20131231003129) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "snaps", ["user_id"], name: "index_snaps_on_user_id"
