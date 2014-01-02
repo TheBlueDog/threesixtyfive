165cc5908bd20105409018832b820249d2683cbc
Erich Rainville <thebluedog@gmail.com>
2014-01-02 15:12:49 -0500

    Start building user index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Threesixtyfive::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "users" => "pages#users"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
