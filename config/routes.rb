28460d09de1fed283aa74296616a61a68097e7e5
TheBlueDog <thebluedog@gmail.com>
2014-01-03 20:25:34 -0500

    Added sticky footer.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Threesixtyfive::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "users" => "pages#users"
+  get "notes" => "pages#notes"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
